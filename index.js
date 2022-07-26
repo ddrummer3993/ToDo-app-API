9655695a707828b7c42938c06a21b19278cdff01
Robert Tai <tairobea@amazon.com>
2022-07-26 09:37:22 -0700

    modify package.json and db post/get

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 import cors from "cors";
 import express from 'express';
-import bodyParser from 'body-parser';
 import { default as mongodb } from 'mongodb';
 let MongoClient = mongodb.MongoClient;
 
 const app = express();
 
+
+app.use(express.json());
+
 app.listen(3000, ()=>{
-    console.log("listening to port 3000");
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) =>{
-    res.sendFile("/Users/tairobea/web_development/star_wars" + '/index.html');
+  console.log("listening to port 3000");
 });
 
 
@@ -21,6 +17,36 @@ app.get('/', (req, res) =>{
 
   MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
-    console.log('Connected to Database')
+    console.log('Connected to Database');
+
+    const db = client.db('taskList');
+    const tasksCollection = db.collection('tasks');
+
+   
+
+    
+
+
+
+   
+    app.post('/toDo', (req, res) => {
+      const task = req.body;
+      tasksCollection.insertOne(task)
+        .then(result => {
+          console.log(task);
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.get('/toDo', (req, res) => {
+      const cursor = db.collection('tasks').find().toArray()
+      .then(results => {
+        console.log(results)
+      })
+      .catch(error => console.error(error))
+    // ...
+  })
+
+
   })
   .catch(error => console.error(error));
